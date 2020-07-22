d6b8c68ee6c70aaf804a9039f42634d53eed1984
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2020-07-22 13:27:06 +0100

    Create aggregate.py
    Beginnings of Python program to do the aggregation for S&B alpha
    workflows.

---
+++ aggregate.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+
+import os
+import sys
+
+import pandas as pd
+
+CONTROL = 0 #Index of main control question determining which workflow we branch into
+
+WORKFLOWS = {
+ 'Index': {
+    'id': 16866,
+    'version': 11.28,
+    'control': 'T20',
+    'other': {
+      'Heading': 'T12',
+      'Subject': ['T13', 'T16', 'T18'],
+      'Pages':   ['T14', 'T17', 'T19'],
+    },
+    'names': {
+      'Title': { 'standard': 'T8', 'other': 'T24' },
+      'Forename': 'T2',
+      'Position': { 'standard': 'T9', 'other': 'T25'},
+      'Subject': 'T26',
+      'Pages': 'T6'
+    },
+    'comments': 'T27',
+  },
+}
+
+parser = argparse.ArgumentParser(
+  description='''Aggregate data from S&B workflows''',
+  epilog="Example: ./aggregate.py",
+  formatter_class=argparse.RawTextHelpFormatter
+)
+parser.add_argument('classifications', nargs='?', default='scarlets-and-blues-classifications.csv', help='Classifications file (default: scarlets-and-blues-classifications.csv)')
+parser.add_argument('-d', '--dump', action='store_true', help='Dump raw JSON')
+args = parser.parse_args()
+
+classifications = pd.read_csv(args.classifications)
+
+for workflow, workflow_data in WORKFLOWS.items():
+  #Heading
+  print(f'### {workflow} {workflow_data["version"]} ({os.path.basename(args.classifications)})')
+
+  workflow_classifications = classifications.loc[(classifications['workflow_id'] == workflow_data['id']) &
+                                                 (classifications['workflow_version'] == workflow_data['version'])]
+  
+  #Read the transcriptions (using our knowledge about the workflows)
+  pages = [json.loads(x) for x in workflow_classifications['annotations']]
+  if args.dump: print(json.dumps(pages, indent=2))
+  for annotations in pages:
+    print(f'Page: ')
+    if annotations[CONTROL]['value'] == 'Other page':
+      print('OTHER')
+
