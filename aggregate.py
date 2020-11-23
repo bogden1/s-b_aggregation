7eb1f2e9487c37ae6c509a33ad4eb4358f91974a
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2020-11-23 20:09:20 +0000

    Refactor: use constants rather than hash, pull index-other logic out to function

--- aggregate.py
+++ aggregate.py
@@ -13,18 +13,6 @@ WORKFLOWS = {
  'Index': {
     'id': 16866,
     'version': 11.28,
-    'control': 'T20',
-    'other': {
-      'Heading': 'T12',
-      'skip': [ 'T15' ], #Tasks in this workflow that we do not process any data for (e.g. control flow)
-      'combo':
-        { 'T11':
-          {
-            'Subject': ['T13', 'T16', 'T18'],
-            'Pages':   ['T14', 'T17', 'T19'],
-          }
-        }
-    },
     'names': {
       'Title': { 'standard': 'T8', 'other': 'T24' },
       'Forename': 'T2',
@@ -32,9 +20,36 @@ WORKFLOWS = {
       'Subject': 'T26',
       'Pages': 'T6'
     },
-    'comments': 'T27',
   },
 }
+
+def index_other(annotations):
+  HEADING = 'T12'
+  SUBJECT_PAGES = 'T11'
+  SUBJECT = ['T13', 'T16', 'T18']
+  PAGES   = ['T14', 'T17', 'T19']
+  SKIP    = 'T15'
+  COMMENTS = 'T27'
+
+  for annotation in annotations:
+    task = annotation['task']
+    value = annotation['value']
+    if task == HEADING: print(value)
+    elif task == SUBJECT_PAGES:
+      for subject_annotation, pages_annotation in zip(value[::2], value[1::2]): #Subject and Pages group pairwise
+        if(not((subject_annotation['task'] in SUBJECT) and
+               (pages_annotation['task']   in PAGES)
+              )
+          ):
+          exit(f'At least one {SUBJECT_PAGES} subtask not in Subject or Pages:\n{subject_annotation}\n{pages_annotation}')
+        value = re.sub(r'^', '  ', subject_annotation['value'], flags = re.MULTILINE)
+        print(value, end=' >>> ')
+        print(pages_annotation['value'])
+        print()
+    elif task == COMMENTS: print(f'Comments: {value}')
+    elif task == SKIP: continue
+    else: exit(f'Unknown task: {task}\n{value}')
+
 
 parser = argparse.ArgumentParser(
   description='''Aggregate data from S&B workflows''',
@@ -71,27 +86,7 @@ for workflow, workflow_data in WORKFLOWS.items():
       print(f'* Page: {page["page"]}')
       control = annotations.pop(0)['value'] #Our workflows all start with a control flow question
       if control == 'Other page':
-        tasks = workflow_data['other']
-        for annotation in annotations:
-          task = annotation['task']
-          value = annotation['value']
-          if task == tasks['Heading']: print(f'{value}')
-          elif task in tasks['combo'].keys():
-            if task == 'T11':
-              for subject_annotation, pages_annotation in zip(value[::2], value[1::2]): #Subject and Pages group pairwise
-                if(not((subject_annotation['task'] in tasks['combo']['T11']['Subject']) and
-                       (pages_annotation['task']   in tasks['combo']['T11']['Pages'])
-                      )
-                  ):
-                  exit(f'T11 subtask not in Subject or Pages:\n{subject_annotation}\n{pages_annotation}')
-                value = re.sub(r'^', '  ', subject_annotation['value'], flags = re.MULTILINE)
-                print(value, end=' >>> ')
-                print(pages_annotation['value'])
-                print()
-            else: error(f'Unknown combo task: {task}\n{value}')
-          elif task == workflow_data['comments']: print(f'Comments: {value}')
-          elif task in tasks['skip']: continue
-          else: exit(f'Unknown task: {task}\nValue: {value}')
+        index_other(annotations)
       elif control == 'Name list':
         tasks = workflow_data['names']
         ##TODO
